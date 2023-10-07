bc2d47566146f1e551fb6eee790f0d9efe654b1e
fsjal-sys <2kpilot2k@gmail.com>
2023-10-07 00:26:45 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pdfkit, requests
+from bs4 import BeautifulSoup
+
+def generate_pdf(limit):
+    count = 1
+
+    while (count <= limit):
+        URL = "https://en.wikipedia.org/wiki/Special:Random"
+        reqs = requests.get(URL)
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+
+        for title in soup.find_all("title"):
+            pdf_title = title.get_text()
+
+        pdfkit.from_url(URL, f"articles/{pdf_title.replace('Wikipedia','')}.pdf")
+
+        print(f"Generated {count} articles.")
+        count += 1
+
+def main():
+    limit = int(input("Number of articles to generate: "))
+    generate_pdf(limit)
+
+if __name__ == '__main__':
+    main()
+
