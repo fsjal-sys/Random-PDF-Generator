9a37f21a4be82622777e2a16b4f31c90a7f30115
fsjal-sys <2kpilot2k@gmail.com>
2023-10-07 00:37:22 -0400

    Add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Random PDF Generator
+
+This Python script utilizes Wikipedia's random article function to generate a specified number of PDF files with diverse content.
+
+## Usage:
+1. Ensure you have the required Python packages installed:
+   - `pdfkit`
+   - `requests`
+   - `beautifulsoup4`
+
+2. Run the following command to generate the PDFs:
+   ```bash
+   python main.py
+   ```
+The generated PDFs will be saved in a directory named 'Articles'. Feel free to modify the limit variable in main.py to control the number of articles to generate. Enjoy exploring a world of random knowledge!
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import pdfkit, requests
+import os
+import pdfkit
+import requests
 from bs4 import BeautifulSoup
 
 def generate_pdf(limit):
     count = 1
 
-    while (count <= limit):
+    # Create the "articles" directory if it doesn't exist
+    if not os.path.exists("Articles"):
+        os.makedirs("Articles")
+
+    while count <= limit:
         URL = "https://en.wikipedia.org/wiki/Special:Random"
         reqs = requests.get(URL)
         soup = BeautifulSoup(reqs.text, 'html.parser')
@@ -12,7 +18,7 @@ def generate_pdf(limit):
         for title in soup.find_all("title"):
             pdf_title = title.get_text()
 
-        pdfkit.from_url(URL, f"articles/{pdf_title.replace('Wikipedia','')}.pdf")
+        pdfkit.from_url(URL, f"Articles/{pdf_title.replace('Wikipedia','')}.pdf")
 
         print(f"Generated {count} articles.")
         count += 1
